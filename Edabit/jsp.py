21d44e7e90f0cfbd64ccd3609fb12e6b109a47c8
Ishak <lekbiramine09@gmail.com>
2026-01-03 16:15:51 +0100

    little change

--- Edabit/jsp.py
+++
@@ -1,9 +0,0 @@
-# hello python again ! 
-# I miss you 
-# Fuck , I am gonna do it this time , and I will succeed ! 
-# No more time wasting !
-# Follow the code 
-# Make money 
-# Learn more 
-# Get bigger
-# Stay Clean 
